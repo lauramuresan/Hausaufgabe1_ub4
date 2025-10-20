8d3c9e750f8406a25925387f949cdffe45c21f79
lauramuresan <lauramuresan420@gmail.com>
2025-10-20 21:28:25 +0300

    exercise 4 second method

--- src/ElectronicsShop.java
+++ src/ElectronicsShop.java
@@ -5,10 +5,24 @@ public class ElectronicsShop {
             throw new IllegalArgumentException("Invalid Input");
 
         int minPrice = KeyboardsPrices[0];
-        for(int i = 1; i < KeyboardsPrices.length; i++){
-            if(KeyboardsPrices[i] < minPrice)
+        for (int i = 1; i < KeyboardsPrices.length; i++) {
+            if (KeyboardsPrices[i] < minPrice)
                 minPrice = KeyboardsPrices[i];
         }
         return minPrice;
+    }
+
+    public int mostExpensiveproduct(int[] KeyboardsPrices, int[] usbPrices) {
+        int maxPrice = KeyboardsPrices[0];
+        for (int i = 1; i < KeyboardsPrices.length; i++) {
+            if(KeyboardsPrices[i] > maxPrice)
+                maxPrice = KeyboardsPrices[i];
+        }
+
+        for (int i = 0; i < usbPrices.length; i++) {
+            if(usbPrices[i] > maxPrice)
+                maxPrice = usbPrices[i];
+        }
+        return maxPrice;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
     public static void main(String[] args) {
         ElectronicsShop electronicsShop = new ElectronicsShop();
         int[] keyboards = {40, 35, 70, 15, 45};
-        System.out.println(electronicsShop.cheapestKeyboard(keyboards));
+        int[] usbs = {20,40,16, 70, 12,87};
+        System.out.println("Cheapest keyboard : " + electronicsShop.cheapestKeyboard(keyboards));
+        System.out.println("Most expensive item : " + electronicsShop.mostExpensiveproduct(keyboards, usbs));
     }
 }
